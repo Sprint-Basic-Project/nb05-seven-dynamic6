d7ccc490b137e3af07ad7ca6bf8699884934b1b0
juho-creator <juhokim2018@gmail.com>
2025-09-18 11:37:16 +0900

    [feat] 테스트 환경 생성함

---
+++ src/02-controller/req-dto/test1.dto.js
@@ -0,0 +1,28 @@
+import { BaseReqDTO } from "./base.req.dto.js";
+
+export class Test1DTO extends BaseReqDTO{
+
+    constructor(data){
+-        super(data);
+    }
+
+    validate()  {
+        const {id, title, content } = this.body;
+        if (!id) {
+            console.log(id);
+            throw Error("아이디를 다시 입력해주세요.");
+        }
+        if (!title) {
+            throw Error("제목을 다시 입력해주세요.");
+        }
+        if (!content) {
+            throw Error("내용을 다시 입력해주세요.");
+        }
+
+        return {
+            id,
+            title,
+            content
+        }
+    }
+}
