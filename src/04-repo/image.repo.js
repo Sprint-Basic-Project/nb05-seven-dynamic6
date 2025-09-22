952422976968af83c8f3c82a2137ebaea76c1ffc
juho-creator <juhokim2018@gmail.com>
2025-09-22 13:09:49 +0900

    [Feat] 이미지 리포지토리

---
+++ src/04-repo/image.repo.js
@@ -0,0 +1,22 @@
+import { groupDummyData, test1Data, test2Data } from "./data/dummy-data.js";
+
+export class ImageRepository {
+  #prisma;
+
+  constructor(prisma) {
+    this.#prisma = prisma;
+  }
+
+  findAll() {
+    return test2Data;
+  }
+
+  save(entity) {
+    test2Data.push({
+      id: entity.id,
+      title: entity.title,
+      content: entity.content,
+    });
+    return entity;
+  }
+}
