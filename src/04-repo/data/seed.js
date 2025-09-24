0cb0f4ad2ae23127cffe5d3ef5d1370915db8456
juho-creator <juhokim2018@gmail.com>
2025-09-24 15:56:58 +0900

    [feat] 데이터 시딩

---
+++ src/04-repo/data/seed.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+import {
+    USERS,
+    GROUPS,
+    USER_JOIN_GROUPS,
+    RECORDS,
+    RECORD_IMAGES,
+    TAGS
+} from "./dummy-data.js";
+
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // await prisma.recordImage.deleteMany();
+    // await prisma.record.deleteMany();
+    // await prisma.userJoinGroup.deleteMany();
+    // await prisma.group.deleteMany();
+    // await prisma.user.deleteMany();
+    // await prisma.tag.deleteMany();
+
+    await prisma.user.createMany({ data: USERS });
+    await prisma.group.createMany({ data: GROUPS });
+    await prisma.userJoinGroup.createMany({ data: USER_JOIN_GROUPS });
+    await prisma.record.createMany({ data: RECORDS });
+    await prisma.recordImage.createMany({ data: RECORD_IMAGES });
+    await prisma.tag.createMany({ data: TAGS });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
