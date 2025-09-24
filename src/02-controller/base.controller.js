cecd09f168adca0d12bd3c8bb43174d8b5d6eb94
juho-creator <juhokim2018@gmail.com>
2025-09-24 15:16:05 +0900

    [Fix] 비동기 콜백 함수도 에러처리하도록 수정

--- src/02-controller/base.controller.js
+++ src/02-controller/base.controller.js
@@ -12,17 +12,12 @@ export class BaseController {
   registerRoutes() {
     throw Error("registerRoutes에 경로를 등록해주세요.");
   }
-  
+
   catchException = (callback) => {
     return (req, res, next) => {
-        try {
-          callback(req, res, next);
-        } catch (err) {
-          next(err);
-        }
-    }
-  }
-
+      Promise.resolve(callback(req, res, next)).catch(next);
+    };
+  };
 
   get basePath() {
     return this.basePath;
