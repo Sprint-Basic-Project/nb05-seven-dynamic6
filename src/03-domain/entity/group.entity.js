0354f1b6570840461e25ce6698997f6a351fd01d
juho-creator <juhokim2018@gmail.com>
2025-09-24 23:32:44 +0900

    [feat] groupEntity 속성 추가함

--- src/03-domain/entity/group.entity.js
+++ src/03-domain/entity/group.entity.js
@@ -1,16 +1,55 @@
 export class Group {
   #id;
   #name;
+  #description;
+  #imageUrl;
+  #goalRep;
+  #discordWebhookUrl;
+  #discordInviteUrl;
   #likeCount;
   #recordCount;
+  #createdAt;
+  #updatedAt;
+  #deletedAt;
   #memberCount;
+  #tags;
+  #owner;
+  #participants;
 
-  constructor({ id, name, likeCount, recordCount, memberCount }) {
+  constructor({
+    id,
+    name,
+    description,
+    imageUrl,
+    goalRep,
+    discordWebhookUrl,
+    discordInviteUrl,
+    createdAt,
+    updatedAt,
+    deletedAt,
+    likeCount,
+    recordCount,
+    memberCount,
+    tags,
+    owner,
+    participants,
+  }) {
     this.#id = id;
+    this.#description = description;
+    this.#imageUrl = imageUrl;
+    this.#goalRep = goalRep;
+    this.#discordWebhookUrl = discordWebhookUrl;
+    this.#discordInviteUrl = discordInviteUrl;
+    this.#createdAt = createdAt;
+    this.#updatedAt = updatedAt;
+    this.#deletedAt = deletedAt;
     this.#name = name;
     this.#likeCount = likeCount;
     this.#recordCount = recordCount;
     this.#memberCount = memberCount;
+    this.#tags = tags;
+    this.#owner = owner;
+    this.#participants = participants;
   }
 
   get id() {
@@ -23,6 +62,58 @@ export class Group {
 
   get likeCount() {
     return this.#likeCount;
+  }
+
+  get description() {
+    return this.#description;
+  }
+
+  get imageUrl() {
+    return this.#imageUrl;
+  }
+
+  get goalRep() {
+    return this.#goalRep;
+  }
+
+  get createdAt() {
+    return this.#createdAt;
+  }
+
+  get updatedAt() {
+    return this.#updatedAt;
+  }
+
+  get deletedAt() {
+    return this.#deletedAt;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get recordCount() {
+    return this.#recordCount;
+  }
+
+  get tags() {
+    return this.#tags;
+  }
+
+  get owner() {
+    return this.#owner;
+  }
+
+  get participants() {
+    return this.#participants;
+  }
+
+  get discordWebhookUrl() {
+    return this.#discordWebhookUrl;
+  }
+  
+  get discordInviteUrl() {
+      return this.#discordInviteUrl;
   }
 
   increaseLike() {
@@ -44,14 +135,5 @@ export class Group {
     if (this.#likeCount >= 100) toAdd.push("LIKE_100");
 
     return { toAdd, toRemove };
-  }
-  toJSON() {
-    return {
-      id: this.#id,
-      name: this.#name,
-      likeCount: this.#likeCount,
-      recordCount: this.#recordCount,
-      memberCount: this.#memberCount,
-    };
   }
 }
