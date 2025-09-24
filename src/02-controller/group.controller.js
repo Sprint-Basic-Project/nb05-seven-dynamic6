56f48298a47c14e8758b0e34c4c7d0165d9741f6
juho-creator <juhokim2018@gmail.com>
2025-09-24 19:21:59 +0900

    [feat] 컨트롤러 추가함

--- src/02-controller/group.controller.js
+++ src/02-controller/group.controller.js
@@ -10,10 +10,25 @@ export class GroupController extends BaseController {
   }
 
   initializeRoutes() {
+    this.router.get("/", this.getAllGroups);
+    this.router.get("/:groupId/rank", this.getGroupRankings);
+
     this.router.post("/:groupId/likes", this.likeGroupMiddleware);
     this.router.delete("/:groupId/likes", this.unlikeGroupMiddleware);
     this.router.delete("/:groupId", this.deleteGroupMiddleware);
   }
+
+  getAllGroups = async (req, res) => {
+    const query = req.query;
+    const result = await this.#service.getGroups(query);
+    return res.status(200).json(result);
+  };
+
+  getGroupRankings = async (req, res) => {
+    const params = req.params;
+    const result = await this.#service.getGroupRankings(params);
+    return res.status(200).json(result);
+  };
 
   likeGroupMiddleware = async (req, res) => {
     const groupId = req.params.groupId;
