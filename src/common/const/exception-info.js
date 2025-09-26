145517902284e48d67eed494643e2c6681795df5
try08253-crypto <try08253@gmail.com>
2025-09-26 12:15:59 +0900

    [fix]Routes,Group명 변경,공용 미들웨어 주입

---
+++ src/common/const/exception-info.js
@@ -0,0 +1,16 @@
+export const EXCEPTION_INFO = {
+  UNKNOWN_SERVER_ERROR: {
+    statusCode: 500,
+    message: "알 수 없는 서버 에러입니다.",
+  },
+
+  GROUP_NOT_FOUND: {
+    statusCode: 404,
+    message: "Group not found",
+  },
+  WRONG_PASSWORD: {
+    statusCode: 401,
+    path: "password",
+    message: "Wrong password",
+  },
+};
---
+++ src/common/middleware/error-handler.js
@@ -0,0 +1,14 @@
+import { Exception } from "../exception/exception.js";
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof Exception) {
+    const { statusCode, message, path } = err;
+    if (path) {
+      return res.status(statusCode).json({ path, message });
+    }
+    return res.status(statusCode).json({ message });
+  }
+
+  console.error(err);
+  return res.status(500).json({ message: "알 수 없는 서버 에러입니다." });
+};
