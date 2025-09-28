c9e6b46bf4403e7a61b35d2aae6cd50b558d168f
gyeongyeonk <gyeongyeonk@gyeongyeonkui-MacBookAir.local>
2025-09-29 00:18:23 +0900

    [feat] user, userjoingroup repository 생성

---
+++ src/04-repo/user-joingroup.repo.js
@@ -0,0 +1,90 @@
+import { UserJoinGroupMapper } from "./mapper/userJoinGroup.mapper.js";
+
+export class UserJoinGroupRepository {
+  #prisma;
+
+  constructor(prisma) {
+    this.#prisma = prisma;
+  }
+
+  async findByUserAndGroup({ userId, groupId }) {
+    const record = await this.#prisma.userJoinGroup.findUnique({
+      where: {
+        groupGroupId_userUserId: {
+          groupGroupId: groupId,
+          userUserId: userId,
+        },
+      },
+    });
+    return UserJoinGroupMapper.toEntity(record);
+  }
+
+  async findByGroupAndNickname({ groupId, nickname }) {
+    const record = await this.#prisma.userJoinGroup.findFirst({
+      where: {
+        groupGroupId: groupId,
+        user: {
+          nickname: nickname,
+        },
+        include: {
+          user: true,
+        },
+      },
+    });
+    return UserJoinGroupMapper.toEntity(record);
+  }
+
+  async create({ userId, groupId }) {
+    const record = await this.#prisma.userJoinGroup.create({
+      data: {
+        groupGroupId: groupId,
+        userUserId: userId,
+      },
+    });
+    return UserJoinGroupMapper.toEntity(record);
+  }
+
+  async delete({ userId, groupId }) {
+    const now = new Date();
+
+    return await this.#prisma.$transaction(async (tx) => {
+      const user = await tx.userJoinGroup.findUnique({
+        where: {
+          groupGroupId_userUserId: {
+            groupGroupId: groupId,
+            userUserId: userId,
+          },
+        },
+        select: {
+          ugId: true,
+        },
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      const deleteRecords = await tx.record.updateMany({
+        where: {
+          userJoinGroupUgId: user.ugId,
+          deletedAt: null,
+        },
+        data: {
+          deletedAt: now,
+        },
+      });
+
+      const deletedUser = await tx.userJoinGroup.update({
+        where: {
+          ugId: user.ugId,
+          deletedAt: null,
+        },
+        data: {
+          deletedAt: now,
+        },
+      });
+
+      return UserJoinGroupMapper.toEntity(deletedUser);
+    });
+  }
+}
