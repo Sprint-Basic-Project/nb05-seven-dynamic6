b0431dad068c2b3bf88baee6eecf0b3e56779d14
gyeongyeonk <gyeongyeonk@gyeongyeonkui-MacBookAir.local>
2025-09-30 10:31:37 +0900

    [fix]auth, userjoingroup 서비스 반환값 dto로 수정, 에러메세지 정리, createUser 닉네임 체크

--- src/domain/service/auth.service.js
+++ src/domain/service/auth.service.js
@@ -1,7 +1,8 @@
-import bcrypt from "bcrypt";
 import { BaseService } from "./base.service.js";
+import { UserResDto } from "../../02-controller/res-dto/user.res.dto.js";
 import { Exception } from "../../common/exception/exception.js";
 import { EXCEPTION_INFO } from "../../common/const/exception-info.js";
+import bcrypt from "bcrypt";
 
 export class AuthService extends BaseService {
   constructor(repos) {
@@ -11,18 +12,32 @@ export class AuthService extends BaseService {
   async authenticateUser({ nickname, password }) {
     const user = await this.repos.userRepo.findByNickname({ nickname });
     if (!user) {
-      throw new Exception(EXCEPTION_INFO.USER_NOT_FOUND);
+      throw new Exception(
+        EXCEPTION_INFO.USER_NOT_FOUND.statusCode,
+        EXCEPTION_INFO.USER_NOT_FOUND.message,
+      );
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
     if (!isPasswordValid) {
-      throw new Exception(EXCEPTION_INFO.INVALID_PASSWORD);
+      throw new Exception(
+        EXCEPTION_INFO.INVALID_PASSWORD.statusCode,
+        EXCEPTION_INFO.INVALID_PASSWORD.message,
+      );
     }
 
     return user;
   }
 
   async createUser({ nickname, password }) {
+    const existingUser = await this.repos.userRepo.findByNickname({nickname})
+
+    if(existingUser) {
+      throw new Exception(
+        EXCEPTION_INFO.NICKNAME_ALREADY_EXISTS.statusCode,
+        EXCEPTION_INFO.NICKNAME_ALREADY_EXISTS.message,
+      )
+    }
     const passwordHash = await bcrypt.hash(password, 10);
 
     const userEntity = User.forCreate({
--- src/domain/service/user-joingroup.service.js
+++ src/domain/service/user-joingroup.service.js
@@ -1,6 +1,7 @@
+import { BaseService } from "./base.service.js";
+import { UserJoinGroupResDto } from "../../02-controller/res-dto/user-joingroup.res.dto.js";
 import { Exception } from "../common/exception/exception.js";
 import { EXCEPTION_INFO } from "../common/exception/exception.js";
-import { BaseService } from "./base.service.js";
 
 export class UserJoinGroupService extends BaseService {
   constructor({ repos, authService }) {
@@ -10,18 +11,21 @@ export class UserJoinGroupService extends BaseService {
 
   async joinGroup({ groupId, nickname, password }) {
     // 그룹 체크
-    const group = await this.repos.groupRepo.findById({ groupId });
-    if (!group) {
-      throw new Exception(EXCEPTION_INFO.GROUP_NOT_EXIST);
+    const groupEntity = await this.repos.groupRepo.findById({ groupId });
+    if (!groupEntity) {
+      throw new Exception(
+        EXCEPTION_INFO.GROUP_NOT_FOUND.statusCode,
+        EXCEPTION_INFO.GROUP_NOT_FOUND.message,
+      );
     }
 
     //User 체크
-    let user = await this.authService.authenticateUser({
+    let userEntity = await this.authService.authenticateUser({
       nickname,
       password,
     });
-    if (!user) {
-      user = await this.authService.create({
+    if (!userEntity) {
+      userEntity = await this.authService.createUser({
         nickname,
         password,
       });
@@ -34,7 +38,10 @@ export class UserJoinGroupService extends BaseService {
         });
 
       if (existingUserJoinGroup && !existingUserJoinGroup.deletedAt) {
-        throw new Exception(EXCEPTION_INFO.ALREADY_JOINED_GROUP);
+        throw new Exception(
+          EXCEPTION_INFO.ALREADY_JOINED_GROUP.statusCode,
+          EXCEPTION_INFO.ALREADY_JOINED_GROUP.message,
+        );
       }
 
       // //닉네임 중복 체크X
@@ -48,40 +55,64 @@ export class UserJoinGroupService extends BaseService {
       // }
 
       //그룹가입
-      return await this.repos.userJoinGroupRepo.create({
-        userId: user.userId,
+      const userJoinGroupEntity = await this.repos.userJoinGroupRepo.create({
+        userId: userEntity.id,
         groupId,
       });
+
+      return new UserJoinGroupResDto(userJoinGroupEntity);
     }
   }
 
   async leaveGroup({ groupId, nickname, password }) {
     //group 체크
-    const group = await this.repos.groupRepo.findById({ groupId });
-    if (!group) {
-      throw new Exception(EXCEPTION_INFO.GROUP_NOT_EXIST);
+    const groupEntity = await this.repos.groupRepo.findById({ groupId });
+    if (!groupEntity) {
+      throw new Exception(
+        EXCEPTION_INFO.GROUP_NOT_FOUND.statusCode,
+        EXCEPTION_INFO.GROUP_NOT_FOUND.message,
+      );
     }
 
     //User 체크
-    const user = await this.authService.authenticateUser({
+    const userEntity = await this.authService.authenticateUser({
       nickname,
       password,
     });
 
+    if (!userEntity) {
+      throw new Exception(
+        EXCEPTION_INFO.USER_NOT_FOUND.statusCode,
+        EXCEPTION_INFO.USER_NOT_FOUND.message,
+      );
+    }
+
     //UserJoinGroup 체크
     const existingUserJoinGroup =
       await this.repos.userJoinGroupRepo.findByUserAndGroup({
-        userId: user.userId,
+        userId: userEntity.id,
         groupId,
       });
     if (!existingUserJoinGroup) {
-      throw new Exception(EXCEPTION_INFO.NOT_A_PARTICIPANT);
+      throw new Exception(
+        EXCEPTION_INFO.PARTICIPANT_NOT_FOUND.statusCode,
+        EXCEPTION_INFO.PARTICIPANT_NOT_FOUND.message,
+      );
     }
 
     //탈퇴(softdelete)
-    return await this.repos.userJoinGroupRepo.delete({
-      userId: user.userId,
+    const deleted = await this.repos.userJoinGroupRepo.delete({
+      userId: userEntity.id,
       groupId,
     });
+
+    if (!deleted) {
+      throw new Exception(
+        EXCEPTION_INFO.LEAVE_GROUP_FAILED.statusCode,
+        EXCEPTION_INFO.LEAVE_GROUP_FAILED.message,
+      );
+    }
+
+    return { message: "그룹 탈퇴가 완료되었습니다" };
   }
 }
