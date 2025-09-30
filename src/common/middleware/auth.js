b99cc29ae4aee16d914ed6dd8c0abef515611b21
try08253-crypto <try08253@gmail.com>
2025-09-30 11:11:26 +0900

    [feat] 비밀번호 검증 추가

--- src/common/middleware/auth.js
+++ src/common/middleware/auth.js
@@ -1,10 +1,11 @@
+import bcrypt from "bcrypt";
 import { Exception } from "../exception/exception.js";
 import { EXCEPTION_INFO } from "../const/exception-info.js";
 
-export const verifyGroupPassword = (groupRepo) => {
+export const verifyGroupPassword = (groupRepo, userRepo) => {
   return async (req, res, next) => {
     const groupId = req.params.groupId;
-    const { ownerPassword } = req.body;
+    const { Password } = req.body;
 
     try {
       const groupEntity = await groupRepo.findById(groupId);
@@ -17,8 +18,20 @@ export const verifyGroupPassword = (groupRepo) => {
         );
       }
 
+      // 유저 조회
+      const userEntity = await userRepo.findById(
+        groupEntity.owner.id ?? groupEntity.userId,
+      );
+      if (!userEntity) {
+        throw new Exception(
+          EXCEPTION_INFO.USER_NOT_FOUND.statusCode,
+          EXCEPTION_INFO.USER_NOT_FOUND.message,
+        );
+      }
+
       // 비밀번호 틀렸을 때 -> 401
-      if (groupEntity.ownerPassword !== ownerPassword) {
+      const isMatch = bcrypt.compareSync(Password, userEntity.passwordHash);
+      if (!isMatch) {
         throw new Exception(
           EXCEPTION_INFO.WRONG_PASSWORD.statusCode,
           EXCEPTION_INFO.WRONG_PASSWORD.message,
